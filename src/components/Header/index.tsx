e5cd354e05a10da079ee04c0102175cd3e666d1a
g-bolsoni <giovanebolsoni2@gmail.com>
2022-05-16 22:27:34 -0300

    :heavy_check_mark: header ready

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,16 @@ const Header: React.FC = () => {
   return (
       <div className="header row">
           <div className="col-lg-12">
+            <div className="container header-elements">
+              <div className="header-img">GMSTORE</div>
+              <div className="header-categories">
+                <ul className="list list-index">
+                  <li className="item">Game</li>
+                  <li className="item">Games</li>
+                  <li className="item">Games</li>
+                </ul>
+              </div>
+            </div>
           </div>
       </div>
   );
